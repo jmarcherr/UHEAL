58756e4ff38221d511a41a44d9a299f7c554b65a
Jonatan Marcher-Rorsted <j.marcher@hotmail.com>
2022-02-21 14:30:45 +0100

    4hz analysis

--- _EEG/_derivatives/_4Hz_results/plot_4hz_processed.m
+++ _EEG/_derivatives/_4Hz_results/plot_4hz_processed.m
@@ -90,328 +90,83 @@ end
 
 YNH_idx = find(age<25 & ~CP);
 ONH_idx = find(age>45 & ~CP);
-
-plot(fft_freq(1,:),squeeze(nanmean(itpc_sub(YNH_idx,:,:))),'k')
-hold on
-plot(fft_freq(1,:),squeeze(nanmean(itpc_sub(ONH_idx,:,:))),'r')
-xlim([0 50])
-%% plotting F stats
+chans = setdiff(1:16,[5 11]);
 close all
-subplot(2,2,[1 2])
-
-%Young
-shadedErrorBar(1:16,nanmean(F_sub(YNH_idx,:)),nanstd(F_sub(YNH_idx,:))/sqrt(length(YNH_idx)),'transparent',0);
+figure('Renderer','painter')
+eb1=shadedErrorBar(fft_freq(1,:),squeeze(nanmean(nanmean(itpc_sub(YNH_idx,chans,:)),2)),...
+    squeeze(nanstd(nanmean(itpc_sub(YNH_idx,chans,:),2)))/sqrt(length(YNH_idx)))
 hold on
-py=plot(1:16,nanmean(F_sub(YNH_idx,:)),'b')
-% old
-shadedErrorBar(1:16,nanmean(F_sub(ONH_idx,:)),nanstd(F_sub(ONH_idx,:))/sqrt(length(ONH_idx)),'transparent',0);
-po=plot(1:16,nanmean(F_sub(ONH_idx,:)),'r')
-fcrit=plot(1:16,F_crit{1}(1:16),'k--')
-
-set(gca,'xtick',1:16,'xticklabel',chan_labels{1}(1:16))
-hleg = legend([py, po,fcrit],'Young','Older','F_{Crit}');
+eb2=shadedErrorBar(fft_freq(1,:),squeeze(nanmean(nanmean(itpc_sub(ONH_idx,chans,:)),2)),...
+   squeeze(nanstd(nanmean(itpc_sub(ONH_idx,chans,:),2)))/sqrt(length(ONH_idx)),'lineprops', 'r')
+xlim([1 20])
+hleg = legend([eb1.mainLine eb2.mainLine],'YNH','ONH');
 hleg.Box = 'off'
-hleg.Position = [0.4738 0.6135 0.1518 0.1238];
-xlabel('Channel')
-ylabel('F-statistic')
-%ylim([4 16.5])
-%ylim([2 18])
-subplot(2,2,3)
-zlim = [80 180];
-c=jm_topoplot(nanmean(F_sub(YNH_idx,:))',zlim,'YNH')
-c.Label.String = 'F-stat';
-%c.Label.Rotation = 0
-
-subplot(2,2,4)
-c=jm_topoplot(nanmean(F_sub(ONH_idx,:))',zlim,'ONH')
-c.Label.String = 'F-stat';
-%c.Label.Rotation = 0
-
-
-set(gcf,'position',[441 318 560 420])
-fig = gcf;
-saveas(fig,'figs2/F_yvo','epsc')
-%% plotting FFR amp
-close all
-subplot(2,2,[1 2])
-
-%Young
-shadedErrorBar(1:16,nanmean(db(FFR_sub(YNH_idx,:))),nanstd(db(FFR_sub(YNH_idx,:)))/sqrt(length(YNH_idx)),'transparent',0);
-hold on
-py=plot(1:16,nanmean(db(FFR_sub(YNH_idx,:))),'b')
-% old
-shadedErrorBar(1:16,nanmean(db(FFR_sub(ONH_idx,:))),nanstd(db(FFR_sub(ONH_idx,:)))/sqrt(length(ONH_idx)),'transparent',0);
-po=plot(1:16,nanmean(db(FFR_sub(ONH_idx,:))),'r')
-%fcrit=plot(1:16,F_crit{1}(1:16),'k--')
-
-set(gca,'xtick',1:16,'xticklabel',chan_labels{1}(1:16))
-hleg = legend([py, po],'Young','Older');
-hleg.Box = 'off'
-hleg.Position = [0.4738 0.6135 0.1518 0.1238];
-xlabel('Channel')
-ylabel('FFR amplitude')
-%ylim([4 16.5])
-%ylim([2 18])
-subplot(2,2,3)
-zlim = [-60 -45];
-c=jm_topoplot(nanmean(db(FFR_sub(YNH_idx,:)))',zlim,'YNH');
-c.Label.String = 'dB mV';
-subplot(2,2,4)
-c=jm_topoplot(nanmean(db(FFR_sub(ONH_idx,:)))',zlim,'ONH');
-c.Label.String = 'dB mV';
-
-
-set(gcf,'position',[441 318 560 420])
-fig = gcf;
-saveas(fig,'figs2/amp_yvo','epsc')
-%% plotting SNR
-close all
-subplot(2,2,[1 2])
-
-%Young
-shadedErrorBar(1:16,nanmean(SNR_sub(YNH_idx,:)),nanstd(SNR_sub(YNH_idx,:))/sqrt(length(YNH_idx)),'transparent',0);
-hold on
-py=plot(1:16,nanmean(SNR_sub(YNH_idx,:)),'b')
-% old
-shadedErrorBar(1:16,nanmean(SNR_sub(ONH_idx,:)),nanstd(SNR_sub(ONH_idx,:))/sqrt(length(ONH_idx)),'transparent',0);
-po=plot(1:16,nanmean(SNR_sub(ONH_idx,:)),'r')
-%fcrit=plot(1:16,F_crit{1}(1:16),'k--')
-
-set(gca,'xtick',1:16,'xticklabel',chan_labels{1}(1:16))
-hleg = legend([py, po],'Young','Older');
-hleg.Box = 'off'
-hleg.Position = [0.4738 0.6135 0.1518 0.1238];
-xlabel('Channel')
-ylabel('SNR (dB)')
-%ylim([4 16.5])
-%ylim([2 18])
-subplot(2,2,3)
-zlim = [30 45];
-c=jm_topoplot(nanmean(SNR_sub(YNH_idx,:))',zlim,'YNH');
-c.Label.String = 'SNR (dB)';
-subplot(2,2,4)
-c=jm_topoplot(nanmean(SNR_sub(ONH_idx,:))',zlim,'ONH');
-c.Label.String = 'SNR (dB)';
-
-
-set(gcf,'position',[441 318 560 420])
-fig = gcf;
-saveas(fig,'figs2/snr_yvo','epsc')
-
-%% Bars F stat
-YNH_non =find((CP==0 & ~isnan(age) & age<23 & sig_idx(:,10)'==0));
-ONH_non = find((CP==0 & ~isnan(age) & age>45 & sig_idx(:,10)'==0));
-close all
-bar(1:2,[nanmean(F_sub(YNH_idx,10))' nanmean(F_sub(ONH_idx,10))'],'FaceColor',[0.6 0.6 0.6])
-hold on
-scatter(ones(size(YNH_idx)),F_sub(YNH_idx,10),'sizedata',85,'XJitter','rand','XJitterwidth',0.2,'marker','.','MarkerEdgeColor','k','MarkerFaceColor','k','markerfacealpha',0.5)
-scatter(ones(size(ONH_idx))*2,F_sub(ONH_idx,10),'sizedata',85,'XJitter','rand','XJitterwidth',0.2,'marker','.','MarkerEdgeColor','r','MarkerFaceColor','r','markerfacealpha',0.5)
-scatter(ones(size(YNH_non)),F_sub(YNH_non,10),'k+')
-scatter(ones(size(ONH_non))*2,F_sub(ONH_non,10),'r+')
-plot([0 3],[F_crit{1}(1:2)],'k--')
-
-set(gca,'xtick',[1 2],'xticklabel',{'YNH','ONH'},'fontsize',14)
 box off
-ylabel('F-Statistic (Cz)')
-set(gcf,'position',[441 502 270 223])
+ylabel('ITPC')
+xlabel('frequency (hz)')
+set(gca,'fontsize',12)
+set(gcf,'position',[441 426 560 276])
+
 fig = gcf;
-saveas(fig,'figs2/f_bar','epsc')
-
-%% bar amp
-%% Bars F stat
-YNH_non =find((CP==0 & ~isnan(age) & age<23 & sig_idx(:,10)'==0));
-ONH_non = find((CP==0 & ~isnan(age) & age>45 & sig_idx(:,10)'==0));
-close all
-bar(1:2,[nanmean(db(FFR_sub(YNH_idx,10)))' nanmean(db(FFR_sub(ONH_idx,10)))'],'FaceColor',[0.6 0.6 0.6])
-hold on
-scatter(ones(size(YNH_idx)),db(FFR_sub(YNH_idx,10)),'sizedata',85,'XJitter','rand','XJitterwidth',0.2,'marker','.','MarkerEdgeColor','k','MarkerFaceColor','k','markerfacealpha',0.5)
-scatter(ones(size(ONH_idx))*2,db(FFR_sub(ONH_idx,10)),'sizedata',85,'XJitter','rand','XJitterwidth',0.2,'marker','.','MarkerEdgeColor','r','MarkerFaceColor','r','markerfacealpha',0.5)
-scatter(ones(size(YNH_non)),db(FFR_sub(YNH_non,10)),'k+')
-scatter(ones(size(ONH_non))*2,db(FFR_sub(ONH_non,10)),'r+')
-%plot([0 3],[F_crit{1}(1:2)],'k--')
-
-set(gca,'xtick',[1 2],'xticklabel',{'YNH','ONH'},'fontsize',14)
-box off
-ylabel('FFR amplitude (Cz)')
-set(gcf,'position',[441 502 270 223])
-fig = gcf;
-saveas(fig,'figs2/amp_bar','epsc')
-
-%% time series
+saveas(fig,'figs/itpc_spec','epsc')
 %%
 close all
-% filter for visualization
-y_ts = squeeze(nanmean(TS_sub(YNH_idx,10,:)));
-o_ts = squeeze(nanmean(TS_sub(ONH_idx,10,:)));
-ts_all = squeeze(nanmean(TS_sub(find(sig_idx(:,10)),10,:)));
-
-filt_coef = [200 400];
-fs = 4096;
-filt_def = designfilt('bandpassfir','FilterOrder',40, ...
-    'CutoffFrequency1',filt_coef(1),'CutoffFrequency2',filt_coef(2), ...
-    'SampleRate',fs);
-y_ts =filtfilt(filt_def,y_ts);
-o_ts = filtfilt(filt_def,o_ts);
-ts_all = filtfilt(filt_def,ts_all);
-t = time(find(time>=0));
-plot(t,y_ts','k')
-hold on
-plot(t,o_ts','r')
-box off
-hleg = legend('YNH','ONH')
-hleg.Box = 'off';
-xlabel('Time [s]');
-ylabel('mV');set(gca,'fontsize',16)
-set(gcf,'position',[441 498 560 227])
-ylim([-0.15 0.15])
-title('')
-fig = gcf;
-saveas(fig,'figs2/ts_yvso','epsc')
-
-
-% all
-
-figure
-plot(t,ts_all,'k')
-xlabel('Time [s]');
-ylabel('mV');set(gca,'fontsize',16)
-set(gcf,'position',[441 498 560 227])
-
-ylim([-0.15 0.15])
-box off
-title(['all sig. n= ' num2str(length(find(sig_idx(:,10))))])
-fig = gcf;
-saveas(fig,'figs2/ts_all','epsc')
-%% plotting spectra
-figure
-plot(fft_freq(1,:),squeeze(nanmean((db(fft_sub(YNH_idx,10,:))))),'k')
-hold on
-plot(fft_freq(1,:),squeeze(nanmean(db(fft_sub(ONH_idx,10,:)))),'r')
-%ylim([-110 -50])
-xlim([250 450])
-box off
-hleg = legend('YNH','ONH')
-hleg.Box = 'off';
-xlabel('Frequency [Hz]');
-ylabel('db mV');set(gca,'fontsize',16)
-set(gcf,'position',[441 498 560 227])
-fig = gcf;
-saveas(fig,'figs2/freq_yvso','epsc')
-
-
-%% age vs. FFR
-close all
-this_idx = find(sig_idx(:,10));
-non_idx = find(sig_idx(:,10)==0);
-scatter(age(this_idx),db(FFR_sub(this_idx,10)),'o','markerfacecolor',[0.6 0.6 0.6],'MarkerEdgecolor','k')
-set(gca,'fontsize',12)
-ll=lsline
-set(ll,'linewidth',2,'color','k')
-hold on
-scatter(age(non_idx),db(FFR_sub(non_idx,10)),'r+')
-ylabel('FFR [dB mV]')
-
-xlabel('Age')
-set(gcf,'Position',[228 420 280 209]);
-[rho,pval]=corr(age(NH_idx)',db(FFR_sub(NH_idx))')
-fig = gcf;
-saveas(fig,'figs2/age_corr_ffr','epsc')
-
-%% F-stat corr
-
-close all
-this_idx = find(sig_idx(:,10));
-non_idx = find(sig_idx(:,10)==0);
-scatter(age(this_idx),F_sub(this_idx,10),'o','markerfacecolor',[0.6 0.6 0.6],'MarkerEdgecolor','k')
-set(gca,'fontsize',12)
-ll=lsline
-set(ll,'linewidth',2,'color','k')
-hold on
-scatter(age(non_idx),F_sub(non_idx,10),'r+')
-ylabel('F-Statistic')
-
-xlabel('Age')
-set(gcf,'Position',[228 420 280 209]);
-plot([10 80], F_crit{1}(:,1:2),'k--')
-[rho,pval]=corr(age(NH_idx)',db(F_sub(NH_idx))')
-fig = gcf;
-saveas(fig,'figs2/age_corr_f','epsc')
-
-%% SNR corr
-
-close all
-this_idx = find(sig_idx(:,10));
-non_idx = find(sig_idx(:,10)==0);
-scatter(age(this_idx),SNR_sub(this_idx,10),'o','markerfacecolor',[0.6 0.6 0.6],'MarkerEdgecolor','k')
-set(gca,'fontsize',12)
-ll=lsline
-set(ll,'linewidth',2,'color','k')
-hold on
-scatter(age(non_idx),SNR_sub(non_idx,10),'r+')
-ylabel('SNR (dB)')
-
-xlabel('Age')
-set(gcf,'Position',[228 420 280 209]);
-plot([10 80], db(F_crit{1}(:,1:2)),'k--')
-[rho,pval]=corr(age(NH_idx)',db(SNR_sub(NH_idx))')
-fig = gcf;
-saveas(fig,'figs2/age_corr_snr','epsc')
-
-
-%% noise floor
-scatter(age(NH_idx),db(noise_sub(NH_idx,10)),'o','markerfacecolor',[0.6 0.6 0.6],'MarkerEdgecolor','k')
-xlabel('Age')
-ylabel('Noise floor (dB mV)')
-ll=lsline
-set(gca,'fontsize',12)
-set(ll,'linewidth',2,'color','k')
-set(gcf,'Position',[228 420 280 209]);
-[rho,pval]=corr(age(NH_idx)',db(noise_sub(NH_idx,10)))
-fig = gcf;
-saveas(fig,'figs2/noise_floor','epsc')
-
-
-
-%% exploratory subtracting noisefloor, then look at amplitudes
-% noise estimation
-close all
-for s=1:size(fft_sub,1)
-c_ffr(s,:) = squeeze(fft_sub(s,10,:)-noise_f_sub(s,10,:));
-FFR_c(s,:) = c_ffr(s,find(fft_freq(1,:)==326));
+figure('renderer','painter')
+top_idx ={YNH_idx,ONH_idx};
+%subplot(1,4,1)
+for fid = [2:2:8]
+    subplot(3,4,fid/2)
+    zlim = [0.1 0.4];
+    jm_topoplot(squeeze(nanmean(itpc_sub(YNH_idx,1:16,find(fft_freq(1,:) == fid))))',zlim,[num2str(fid) ' Hz'],0);
+    
+    subplot(3,4,fid/2+4)
+    jm_topoplot(squeeze(nanmean(itpc_sub(ONH_idx,1:16,find(fft_freq(1,:) == fid))))',zlim,[num2str(fid) ' Hz'],0);
 end
-
-%plotting spectra
-
-plot(fft_freq(1,:),squeeze(nanmean((c_ffr(YNH_idx,:)))),'k')
-hold on
-plot(fft_freq(1,:),squeeze(nanmean(c_ffr(ONH_idx,:))),'r')
-%ylim([-110 -50])
-xlim([250 450])
-box off
-hleg = legend('YNH','ONH')
-hleg.Box = 'off';
-xlabel('Frequency [Hz]');
-ylabel('db mV');set(gca,'fontsize',16)
-set(gcf,'position',[441 498 560 227])
+subplot(3,4,12)
+c=jm_topoplot(zeros(16,1),zlim,'scale',1)
+c.Label.String = 'itpc';
 fig = gcf;
-saveas(fig,'figs2/freq_yvso_corrected','epsc')
+saveas(fig,'figs/itpc_top','epsc')
 
-%% FFR corrected
 
+%% time series
 close all
-this_idx = find((sig_idx(:,10)' & ~isnan(age)));
-non_idx = find(sig_idx(:,10)==0);
-scatter(age(this_idx),db(FFR_c(this_idx)),'o','markerfacecolor',[0.6 0.6 0.6],'MarkerEdgecolor','k')
+figure('Renderer','painter')
+eb1=shadedErrorBar(time,squeeze(nanmean(nanmean(TS_sub(YNH_idx,chans,:)),2)),...
+    squeeze(nanstd(nanmean(TS_sub(YNH_idx,chans,:),2)))/sqrt(length(YNH_idx)))
+hold on
+eb2=shadedErrorBar(time,squeeze(nanmean(nanmean(TS_sub(ONH_idx,chans,:)),2)),...
+   squeeze(nanstd(nanmean(TS_sub(ONH_idx,chans,:),2)))/sqrt(length(ONH_idx)),'lineprops', 'r')
+%xlim([1 20])
+hleg = legend([eb1.mainLine eb2.mainLine],'YNH','ONH');
+hleg.Box = 'off'
+hleg.Position = [0.8381 0.5020 0.1500 0.2978];
+box off
+ylabel('mV')
+xlabel('time (s)')
 set(gca,'fontsize',12)
+set(gcf,'position',[441 426 560 276])
+hA=gca;
+hA.XRuler.MinorTick ='On';
+%set(gcf,'position',[441 566 560 136])
+set(gcf,'position',[441 587 560 115])
+fig = gcf;
+saveas(fig,'figs/itpc_ts','epsc')
+
+%% age vs. ITPC frequencies
+fid = [2:2:8];
+close all
+figure('renderer','painter')
+for ii=1:length(fid)
+    subplot(2,2,ii)
+scatter(age,mean(itpc_sub(:,chans,find(fft_freq(1,:)==fid(ii))),2)','o','markerfacecolor',[0.6 0.6 0.6],'MarkerEdgecolor','k')
+set(gca,'fontsize',12)
+title([num2str(fid(ii)) ' Hz'])
 ll=lsline
 set(ll,'linewidth',2,'color','k')
-hold on
-scatter(age(non_idx),db(FFR_c(non_idx)),'r+')
-ylabel('FFR corrected-amp')
-
-xlabel('Age')
-set(gcf,'Position',[228 420 280 209]);
-%plot([10 80], F_crit{1}(:,1:2),'k--')
-[rho,pval]=corr(age(this_idx)',db(FFR_c(this_idx)))
+ylim([0 .8])
+[rho(ii),pval(ii)]=corr(age',mean(itpc_sub(:,chans,find(fft_freq(1,:)==fid(ii))),2))
+end
+set(gcf,'position',[441 244 421 458])
 fig = gcf;
-saveas(fig,'figs2/age_corr_ffr_corrected','epsc')
+saveas(fig,'figs/age_itpc','epsc')
--- _scripts/jm_topoplot.m
+++ _scripts/jm_topoplot.m
@@ -1,4 +1,4 @@
-function c=jm_topoplot(var1,zlim,tit_string)
+function c=jm_topoplot(var1,zlim,tit_string,coff)
 load('/work1/jonmarc/UHEAL_master/UHEAL/_EEG/_func/topo_default.mat');
 freq.powspctrm = var1;%nanmean(F_sub(YNH_idx,:))';
 cfg = [];
@@ -10,9 +10,11 @@ cfg.channel = freq.cfg.channel;
 cfg.parameter = 'powspctrm';
 cfg.style = 'straight';
 cfg.zlim = zlim;
-ft_topoplotER(cfg,freq); colorbar
+ft_topoplotER(cfg,freq);
 title(tit_string)
 ft_hastoolbox('brewermap', 1);         % ensure this toolbox is on the path
 colormap(brewermap(64,'YlOrRd')) % change the colormap
+if coff
 c=colorbar;
+end
 end
